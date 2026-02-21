75c1545d36215fa2184a01b3188d50c379e4615e
captionAdy <groupadrespect@gmail.com>
2026-02-21 15:58:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const multer = require("multer");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+app.use(express.json());
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 app.get("/", (req, res) => {
   res.send("ResuTransformer backend running 🚀");
+});
+
+app.get("/test", (req, res) => {
+  res.json({ status: "Server working perfectly" });
 });
 
 app.post("/upload", upload.single("resume"), (req, res) => {
